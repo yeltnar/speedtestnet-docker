168682f5fc55845a4750e914b6cfb3184c2f706b
Andrew Brantley <github@andbrant.com>
2022-09-24 10:58:14 -0500

    fix timestamp

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ if [ ! -e "$out_dir" ]; then
 	mkdir -p "$out_dir";
 fi
 
-docker build -t speedtest . && docker run -i speedtest > "$out_dir/speedtest_$(date '+%F_%T').txt"
+docker build -t speedtest . && docker run -it speedtest > "$out_dir/speedtest_$(date '+%F_%T' | sed s/:/-/g).txt"
