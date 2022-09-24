16e20a2e36da54e6d307c9f8456e759f12f0e2f2
Andrew Brantley <github@andbrant.com>
2022-09-24 10:21:27 -0500

    add directory (so we can symlink somewhere else)

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-docker build -t speedtest . && docker run -i speedtest > "speedtest_$(date '+%F_%T').txt"
+out_dir="reports";
+
+if [ ! -e "$out_dir" ]; then
+	mkdir -p "$out_dir";
+fi
+
+docker build -t speedtest . && docker run -i speedtest > "$out_dir/speedtest_$(date '+%F_%T').txt"
