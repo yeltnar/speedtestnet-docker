afafa3e2119ef6e71348705133639500dedb2e51
Andrew Brantley <github@andbrant.com>
2022-09-24 12:51:35 -0500

    add tz

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ if [ ! -e "$out_dir" ]; then
 	mkdir -p "$out_dir";
 fi
 
-docker build -t speedtest . && docker run -t speedtest > "$out_dir/speedtest_$(date '+%F_%T' | sed s/:/-/g).txt"
+docker build -t speedtest . && docker run -t -e TZ=America/Chicago speedtest > "$out_dir/speedtest_$(date '+%F_%T' | sed s/:/-/g).txt"
